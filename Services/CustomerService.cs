74a8c1d192eb13ba8ffea54fc32a6b66ebd45c12
toeitoei18 <hs4toei2544@gmail.com>
2024-09-17 10:49:15 +0700

    Update

--- Services/CustomerService.cs
+++ Services/CustomerService.cs
@@ -18,6 +18,32 @@ namespace BlazorServerDemo.Services
             using(var context = _dbContextFactroy.CreateDbContext())
             {
                 context.Customers.Add(customer);
+                context.SaveChanges();
+            }
+        }
+
+        public Customer GetCustomerByName(string name) 
+        {
+            using(var context = _dbContextFactroy.CreateDbContext())
+            {
+                var customer = context.Customers.SingleOrDefault(x => x.Name == name);
+                return customer;
+            }
+        }
+
+//สอนเอาไว้//
+        public void UpdateCustomerByName(string name, int age)
+        {
+            var customer = GetCustomerByName(name);
+            if(customer == null)
+            {
+                throw new Exception("Customer doess not exist. Cannot update");
+            }
+            customer.Age = age;
+            using(var context = _dbContextFactroy.CreateDbContext())
+            {
+                context.Update(customer);
+                 context.SaveChanges();
             }
         }
 
